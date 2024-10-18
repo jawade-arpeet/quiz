ea6d7e3ccb1eb5bf74f46542eec9e7f4d31eb1c1
jawade.arpeet <jawade.arpeet@gmail.com>
2024-10-18 14:54:30 +0530

    add request handler for /api/result

--- src/app/api/result/route.ts
+++ src/app/api/result/route.ts
@@ -1,5 +1,6 @@
 import { responses } from "@/app/db/db";
 import { NextResponse } from "next/server";
+
 function GET() {
   const filteredResponses = responses.filter(response => response.answer === response.response);
   return NextResponse.json({correct: filteredResponses.length, incorrect: responses.length - filteredResponses.length}, {status: 200});
