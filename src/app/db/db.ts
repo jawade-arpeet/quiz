b9fab11fbd0e74e2644e50ace8f68644dfbcf704
jawade.arpeet <jawade.arpeet@gmail.com>
2024-10-18 13:48:18 +0530

    add in memory database

---
+++ src/app/db/db.ts
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is the capital of India?",
+    options: ["Delhi", "London", "Berlin", "Rome"],
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "What is the capital of Germany?",
+    options: ["Berlin", "London", "Paris", "Rome"],
+    answer: "Berlin",
+  },
+  {
+    id: 3,
+    question: "What is the capital of Italy?",
+    options: ["Rome", "London", "Paris", "Berlin"],
+    answer: "Rome",
+  },
+  {
+    id: 4,
+    question: "What is the capital of Spain?",
+    options: ["Madrid", "London", "Paris", "Berlin"],
+    answer: "Madrid",
+  },
+  {
+    id: 5,
+    question: "What is the capital of Sweden?",
+    options: ["Stockholm", "London", "Paris", "Berlin"],
+    answer: "Stockholm",
+  },
+];
+
+const responses = [];
+
+export { questions, responses };
